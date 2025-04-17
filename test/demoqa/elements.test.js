c63084233afd202be9678ac8faa420a05ec9ac26
Sindhuja Martha <sindhuja90compscienceprograms@gmail.com>
2025-04-17 15:34:45 -0500

    add tests - checkbox, radiobutton, webtables in elements

--- test/demoqa/elements.test.js
+++ test/demoqa/elements.test.js
@@ -1,4 +1,5 @@
 const {test,expect} = require('@playwright/test');
+
 test("textbox test",async({page}) => {
     await page.goto('https://demoqa.com/text-box');
     await page.locator('#userName').fill('John');
@@ -15,6 +16,40 @@ test("textbox test",async({page}) => {
     await expect(email).toHaveText('Email:john123@gmail.com');
     await expect(currentAddress).toHaveText('Current Address :#123,SpringCreek,Portland');
     await expect(permanentAddress).toHaveText('Permananet Address :#1234,Rose street, Denver');
+})
+
+test("checkbox test", async({page})=> {
+    await page.goto('https://demoqa.com/checkbox');
+    await page.locator('//span[@class="rct-title"]').check();
+
+})
+
+test("radiobutton test",async({page})=> {
+    await page.goto('https://demoqa.com/radio-button');
+    await page.locator('//label[@for="impressiveRadio"]').check();
+    await page.locator('//label[@for="yesRadio"]').check();
+
+})
+
+test("webtables test",async({page})=> {
+    await page.goto('https://demoqa.com/webtables');
+    await page.locator('#addNewRecordButton').click();
+    await page.locator('#firstName').fill('John');
+    await page.locator('#lastName').fill('Cena');
+    await page.locator('#userEmail').fill('john@gmail.com');
+    await page.locator('#age').fill('25');
+    await page.locator('#salary').fill('800000');
+    await page.locator('#department').fill('IT');
+    await page.locator('#submit').click();
     
+    await page.locator('//div[contains(text(),"Fir")]').click();
+    await page.locator('//div[contains(text(),"Last")]').click();
+    await page.locator('//div[contains(text(),"Age")]').click();
+    await page.locator('//div[contains(text(),"Ema")]').click();
+    await page.locator('//div[contains(text(),"Salary")]').click();
+    await page.locator('//div[contains(text(),"Depa")]').click();
+    await page.locator('//div[contains(text(),"Act")]').click();
+    await page.locator('#searchBox').fill('test');
+
 
 })
